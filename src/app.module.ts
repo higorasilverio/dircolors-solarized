3e104fc62ca0a15851af8d4bcdbf7764111526b3
higorasilverio <higorasilverio@outlook.com>
2022-08-19 16:36:50 -0300

    ✨ (logs): winston log manager added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
+import { winstonConfig } from './configs/winston.config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { LeadsModule } from './leads/leads.module';
+import { WinstonModule } from 'nest-winston';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), LeadsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
+    LeadsModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
