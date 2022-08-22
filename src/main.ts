279f96b1bef196174c217964b6986c013c1a2977
higorasilverio <higorasilverio@outlook.com>
2022-08-21 21:52:36 -0300

    🐛 (main): cors enabled

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { winstonConfig } from './configs/winston.config';
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonConfig);
   const app = await NestFactory.create(AppModule, { logger });
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(8000);
 }
 bootstrap();
